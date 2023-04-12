96fac8cb60e717b20aaa9b833550d03f07d649f6
Sean Johnson <seanjohn@udel.edu>
2023-04-12 12:04:18 -0400

    Imported and installed networkx, decided on using prims algorithm to solve the problem

---
+++ prims.py
@@ -0,0 +1,13 @@
+import networkx as nx
+
+"""
+Done by Sean Johnson, seanjohn@udel.edu
+
+The problem I have created is 
+
+The Algorithm I am using on the problem is Prim's algorithm in order to create a minimal spanning tree.
+
+I am Using networkx in order to create the graph
+
+
+"""
