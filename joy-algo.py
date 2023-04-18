a61d9dffca2cc5a1c9bc963e9e9c194f39729f56
Joy <jkmwaria@udel.edu>
2023-04-17 21:34:10 -0400

    added my info to index.md, created file for my graph problem and copied in outline for problem

--- joy-algo.py
+++ joy-algo.py
@@ -1,33 +1,11 @@
-# University of Delaware Graphs
-## Theme: Campus Life
-
-**CISC320 Spring 2023 Lesson 19 - Graph Applications**
-
-Group Members:
-* First member: Sean Johnson, (seanjohn@udel.edu)
-* Second member: Joy Mwaria (jkmwaria@udel.edu)
-* Third member (email)
-* Fourth member (email)
-
-Description of project
-
-## Installation Code
-
-```sh
-$> pip install networkx
-$> pip install matplotlib
-```
-
-## Python Environment Setup
-
-```python
 import networkx as nx
 import matplotlib.pyplot as plt
-```
 
-### Minimal Brick Walkways
 
-**Informal Description**: 
+# Problem Name
+
+"""
+Informal Description: 
 The problem: The sidewalks to the library at UD have been declared unfit for use due to wear and decay. The university wants to build brick walkways this time instead, but they're very expensive! The university has many interconnected sidewalks, but this is too expensive to do for brick walkways, so UD wants to have the minimum number of walkways in order to get from the buildings on campus to the library. As the engineer contracted by UD your job is to cut costs to a minimum for the university. You are to use the smallest amount of bricks possible   (A length of 1 in the context of the problem is equivalent to a 100ft length walkway of bricks) in order to make sure every major building has a path to the library. You are also using the previous network of interconnected sidewalks(the visualized graph) to decide where to place your brick walkways.
 
 > **Formal Description**:
@@ -35,11 +13,11 @@ The problem: The sidewalks to the library at UD have been declared unfit for use
 >  * Output: The minimum spanning tree of that graph
 
 **Graph Problem/Algorithm**: MST/Prim's
+"""
 
+# Setup code:
 
-**Setup code**:
-
-```python
+"""
 graph = {
     'L': {'KB': {'weight': 4}, 'SH': {'weight': 8}, 'MH': {'weight': 1}, 'CR': {'weight': 4},
                'AHW': {'weight': 2}, 'AH': {'weight': 2}, 'TH': {'weight': 9}},
@@ -70,18 +48,15 @@ plt.figure(1,figsize=(12,12))
 nx.draw_networkx(G,pos, node_size=60, font_size=8)
 nx.draw_networkx_edge_labels(G,pos,edge_labels=labels)
 plt.savefig("initial_graph.png")
+"""
 
+# Visualization:
 
+# ![A visualization of the initial graph](initial_graph.png)
 
-```
+# Solution code:
 
-**Visualization**:
-
-![A visualization of the initial graph](initial_graph.png)
-
-**Solution code:**
-
-```python
+"""
 H = nx.algorithms.minimum_spanning_tree(G, weight = "weight", algorithm = "prim", ignore_nan = False)
 
 pos = nx.spring_layout(H, seed=4)
@@ -96,6 +71,9 @@ plt.savefig("minimum_spanning_tree")
 **Output**
 
 ![The resulting minimum spanning tree](minimum_spanning_tree.png)
+"""
 
-**Interpretation of Results**:
-The resulting subgraph is the minimum spanning tree, which is the subgraph with the least total edge weight while still visiting all the vertices. In the context of this problem, it is the graph of the brick walkway network to each UD building on the graph. This graph uses the least amount of bricks possible and has a way to get from every vertex to the library. 
+# Interpretation of Results:
+# The resulting subgraph is the minimum spanning tree, which is the subgraph with the least total edge weight while still 
+# visiting all the vertices. In the context of this problem, it is the graph of the brick walkway network to each UD building 
+# on the graph. This graph uses the least amount of bricks possible and has a way to get from every vertex to the library. 
